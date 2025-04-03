fa9f0e32429d9e20bdf5e85a6d5c2bf34fc0e255
Mega17Z <planimportant@gmail.com>
2025-04-03 20:38:19 +0000

    Finition

--- src/Page d'accueil/BoutiqueProfil/boutique.js
+++ src/Page d'accueil/BoutiqueProfil/boutique.js
@@ -1,17 +1,22 @@
 import React from "react";
 import './boutique.css'
 import Vendeur from "../Vendeur/vendeur.js";
-import Image from "../image/1.jpg"
+import logo1 from "../image/logo1.png"
+import logo2 from "../image/logo2.png"
+import logo3 from "../image/logo3.png"
+import logo4 from "../image/logo4.png"
+import logo5 from "../image/logo5.png"
+import logo6 from "../image/logo6.png"
 
 const BoutiquePart = () => {
     return ( 
         <div className="boutique">
-            <Vendeur image={Image} nom="Beugue Fallou" />
-            <Vendeur image={Image} nom="Nafina Shop" />
-            <Vendeur image={Image} nom="Yamal Boutique" />
-            <Vendeur image={Image} nom="Sope Naby" />
-            <Vendeur image={Image} nom="Jum Store" />
-            <Vendeur image={Image} nom="Tapha Chaussures" />
+            <Vendeur image={logo1} nom="Beugue Fallou" />
+            <Vendeur image={logo2} nom="Nafina Shop" />
+            <Vendeur image={logo3} nom="Yamal Boutique" />
+            <Vendeur image={logo4} nom="Sope Naby" />
+            <Vendeur image={logo5} nom="Jum Store" />
+            <Vendeur image={logo6} nom="Tapha Chaussures" />
         </div>
     );
 }
--- src/Page d'accueil/Infos du Site/infos.js
+++ src/Page d'accueil/Infos du Site/infos.js
@@ -5,9 +5,9 @@ import PartInfos from "../part Infos/infoPart";
 const InfoSection = () => {
     return ( 
         <div className="infos">
-            <PartInfos titre="Lieu" details="Dakar, Sénégal" />
-            <PartInfos titre="Contact" details="33 333 33 33 / 77 777 77 77" />
-            <PartInfos titre="Mail" details="nom@gmail.com" />
+            <PartInfos titre="Lieu" details="Dakar, Sénégal" icone={<i class="fa-solid fa-location-dot"></i>} />
+            <PartInfos titre="Contact" details="33 333 33 33 / 77 777 77 77" icone={<i class="fa-solid fa-phone"></i>} />
+            <PartInfos titre="Mail" details="nom@gmail.com" icone={<i class="fa-solid fa-envelope"></i>} />
         </div>
     );
 }
